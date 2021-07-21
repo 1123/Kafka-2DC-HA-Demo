bd1085e142d7787361aaa72b547bf5609aa134a1
Benedikt Linse <benedikt@confluent.io>
2021-07-21 15:32:44 +0200

    adjusting default replicaiton factor, default min.isr

---
+++ delete-topic.sh
@@ -0,0 +1,3 @@
+kafka-topics --delete \
+  --topic test \
+  --bootstrap-server localhost:9091,localhost:9092,localhost:9093,localhost:9094 
--- run-producer.sh
+++ run-producer.sh
@@ -1,8 +1,6 @@
 kafka-producer-perf-test \
   --throughput 10 \
   --record-size 1024 \
-  --num-records 200 \
+  --num-records 20000 \
   --topic test \
-  --producer-props \
-    bootstrap.servers=localhost:9091,localhost:9092,localhost:9093,localhost:9094 \
-    acks=-1
+  --producer.config producer.config 
