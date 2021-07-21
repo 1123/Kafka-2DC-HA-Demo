fdc7f80e5674ff30708fcb24a48dafc6d886635b
Benedikt Linse <benedikt@confluent.io>
2021-07-21 12:32:56 +0200

    adding script for getting Zookeeper broker modes

---
+++ get-modes.sh
@@ -0,0 +1,6 @@
+echo srvr | nc localhost 2181
+echo srvr | nc localhost 2182
+echo srvr | nc localhost 2183
+echo srvr | nc localhost 2184
+echo srvr | nc localhost 2185
+echo srvr | nc localhost 2186
