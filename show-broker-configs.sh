cfd9c4d9e22c91d80552c791342ae5d10b752dd0
Benedikt Linse <benedikt@confluent.io>
2021-07-22 09:33:23 +0200

    bugfix

--- show-broker-configs.sh
+++ show-broker-configs.sh
@@ -3,5 +3,5 @@ for broker in 1 2 3 4; do
     --describe \
     --entity-type brokers \
     --bootstrap-server localhost:9091,localhost:9092,localhost:9093,localhost:9094 \
-    --entity-name 1 
+    --entity-name $broker
 done
